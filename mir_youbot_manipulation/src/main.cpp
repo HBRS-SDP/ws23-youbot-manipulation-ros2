f8b7d0035b84f2bca53fd66744bf8764807d98c9
Chaitanya Gumudala <chaitanyagumudala961@gmail.com>
2023-11-10 14:38:28 +0100

    Offset and publish

--- mir_youbot_manipulation/src/main.cpp
+++ mir_youbot_manipulation/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include "youbot_driver/youbot/EthercatMasterWithThread.hpp"
 #include "youbot_driver/youbot/YouBotManipulator.hpp"
 #include "youbot_driver/youbot/DataTrace.hpp"
@@ -14,34 +15,50 @@ bool isinRange(double value, double minimum, double maximum){
 }
 
 int main(int argc, char **argv) {
-    // EthercatMaster::getInstance("youbot-ethercat.cfg", ethercat_config_path, true);
-    // YouBotManipulator myArm("youbot-manipulator", ethercat_config_path);
-    // myArm.doJointCommutation();
-    // myArm.calibrateManipulator();
+    EthercatMaster::getInstance("youbot-ethercat.cfg", ethercat_config_path, true);
+    YouBotManipulator myArm("youbot-manipulator", ethercat_config_path);
+    myArm.doJointCommutation();
+    myArm.calibrateManipulator();
     double minimum_angles[] = {-2.94961, -1.13446, -2.63545, -1.78896248, -2.879793};
     double maximum_angles[] = {2.94961, 1.5708, 2.54818, 1.78896248, 2.87979};
 
-    std::vector<double> inputAngles;
+    double q_offsets[5];
+    q_offsets[0] = (169.0 * M_PI) / 180.0;
+    q_offsets[1] = 65.0 * M_PI / 180.0;
+    q_offsets[2] = -151.0 * M_PI / 180.0;
+    q_offsets[3] = 102.5 * M_PI / 180.0;
+    q_offsets[4] = 65.0 * M_PI / 180.0;
+    
+
+    std::vector<JointAngleSetpoint> inputAngles;
+    std::vector<JointSensedAngle> sensedAngles;
+    
+    myArm.getJointData(sensedAngles);
+    for (int i = 0; i < 5; i++) {
+        std::cout << sensedAngles[i].angle.value() - q_offsets[i] << std::endl;
+    }
+    
+
 
     std::cout << "Enter joint angles(in radians)" << std::endl;
 
-    for (int i; i < 5; i++) {
+    for (int i=0; i < 5; i++) {
         double angle;
-        std::cout << "Enter joint angle" << i + 1 << " : ";
+        JointAngleSetpoint inputangle;
+        
+        std::cout << "Enter joint angle-" << i + 1 << " : ";
         std::cin >> angle;
+        inputangle.angle = (angle + q_offsets[i]) * radian;
 
         if (isinRange(angle, minimum_angles[i], maximum_angles[i])) {
-            inputAngles.push_back(angle);
+            inputAngles.push_back(inputangle);
         } else {
             std::cout << "Wrong limits" << std::endl;
             --i;
         }
     }
-    
-    std::cout << "Entered joint angles: ";
-    for (const auto& angle : inputAngles) {
-        std::cout << angle << " ";
-    }
-    std::cout << std::endl;
+    std::cout << "Moving robot to the desired joint angles" << std::endl;
+    myArm.setJointData(inputAngles);
+    sleep(5);
     return 0;
 }
