9f1b36ca38a08e4a287f8e7f048174bd8fe78d9d
Chaitanya Gumudala <chaitanyagumudala961@gmail.com>
2023-11-27 13:16:13 +0100

    Life Cycle Node

--- mir_youbot_manipulation/include/mir_youbot_manipulation/youbot_manipulation_node.hpp
+++ mir_youbot_manipulation/include/mir_youbot_manipulation/youbot_manipulation_node.hpp
@@ -7,36 +7,30 @@
 
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "brics_actuator/msg/joint_positions.hpp"
 #include "mir_youbot_manipulation/youbot_manipulation.hpp"
 
 
-namespace manipulation_ros_namespace {
-	class ManipulatorROS: public rclcpp_lifecycle::LifecycleNode{ 
+class ManipulatorRosNode: public rclcpp_lifecycle::LifecycleNode
+{ 
 	public: 
- 		explicit ManipulatorROS(const rclcpp::NodeOptions& options); 
+ 		explicit ManipulatorRosNode(const rclcpp::NodeOptions& options); 
+
+		virtual ~ManipulatorRosNode();
   
  		/// Transition callback for state configuring 
- 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn 
- 		on_configure(const rclcpp_lifecycle::State &); 
+ 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_configure(const rclcpp_lifecycle::State&); 
   
  		/// Transition callback for state activating 
- 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn 
- 		on_activate(const rclcpp_lifecycle::State & state); 
+ 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_activate(const rclcpp_lifecycle::State& state); 
   
  		/// Transition callback for state deactivating 
- 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn 
- 		on_deactivate(const rclcpp_lifecycle::State & state); 
-  
- 		  /// Transition callback for state cleaningup 
- 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn 
- 		on_cleanup(const rclcpp_lifecycle::State &); 
+ 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State& state); 
+ 		  
+		/// Transition callback for state cleaningup 
+ 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_cleanup(const rclcpp_lifecycle::State&); 
   
  		/// Transition callback for state shutting down 
- 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn 
- 		on_shutdown(const rclcpp_lifecycle::State & state);
-	private:
-		
-
-	};
-}
+ 		rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_shutdown(const rclcpp_lifecycle::State& state);
+};
--- mir_youbot_manipulation/src/youbot_manipulation_node.cpp
+++ mir_youbot_manipulation/src/youbot_manipulation_node.cpp
@@ -5,19 +5,65 @@
 *
 */
 
-#include "rclcpp/rclcpp.hpp"
-#include "brics_actuator/msg/joint_positions.hpp"
-#include "mir_youbot_manipulation/youbot_manipulation.hpp"
+#include "mir_youbot_manipulation/youbot_manipulation_node.hpp"
+
+ManipulatorRosNode::ManipulatorRosNode(const rclcpp::NodeOptions& options) : rclcpp_lifecycle::LifecycleNode("youbot_manipulation_node", options)
+{
+    RCLCPP_INFO(get_logger(), "Manipulator Node is created");
+    auto overrides = this->get_node_options().parameter_overrides();
+    for (auto& override : overrides) {
+        declare_parameter(override.get_name(), override.get_parameter_value());
+    }
+}
+
+ManipulatorRosNode::~ManipulatorRosNode() 
+{
+    RCLCPP_INFO(get_logger(), "Manipulator Node is destroyed");
+}
+
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn ManipulatorRosNode::on_configure(const rclcpp_lifecycle::State&) 
+{
+    RCLCPP_INFO(get_logger(), "Manipulator Node configured");
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+
+}
+
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn ManipulatorRosNode::on_activate(const rclcpp_lifecycle::State& state) 
+{
+    RCLCPP_INFO(get_logger(), "Manipulator Node is activated");
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+
+}
+
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn ManipulatorRosNode::on_deactivate(const rclcpp_lifecycle::State& state)
+{
+    RCLCPP_INFO(get_logger(), "Manipulator Node is de-activated");
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+
+}
+
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn ManipulatorRosNode::on_cleanup(const rclcpp_lifecycle::State&)
+{
+    RCLCPP_INFO(get_logger(), "Manipulator Node cleaned up");
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+
+}
+
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn ManipulatorRosNode::on_shutdown(const rclcpp_lifecycle::State &state)
+{
+    RCLCPP_INFO(get_logger(), "Manipulator Node shutdown");
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("mir_youbot_manipulation");
+    auto node = std::make_shared<ManipulatorRosNode>(rclcpp::NodeOptions());
     auto ethercat_path = ament_index_cpp::get_package_share_directory("youbot_driver");
     string file_path = ethercat_path + "/config";
     manipulation_namespace::Manipulator manipulator(file_path);
     RCLCPP_INFO(node->get_logger(), "Your ROS wrapper node is working.");
-    rclcpp::spin(node);
+    rclcpp::spin(node->get_node_base_interface());
     rclcpp::shutdown();
     return 0;
-
 }
