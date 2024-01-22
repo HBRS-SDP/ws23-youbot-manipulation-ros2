6f22464e5d460c7ff295c6375e1c78f61cde8e7f
Chaitanya Gumudala <chaitanyagumudala961@gmail.com>
2024-01-22 14:42:50 +0100

    ..

--- mir_youbot_manipulation/src/youbot_manipulation_node.cpp
+++ mir_youbot_manipulation/src/youbot_manipulation_node.cpp
@@ -287,84 +287,10 @@ void ManipulatorRosNode::executeJointVelocities(
   tf2::fromMsg(cartesian_coordinates.pose, target_pose);
 }
 
-double calculateVelocity(double amplitude, double start_pose, double target_pose,
-                         double current_pose)
-{
-  return amplitude * std::sin((M_PI / (target_pose - start_pose)) * current_pose);
-}
-
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<ManipulatorRosNode>(rclcpp::NodeOptions());
-  KDL::Tree youbot_tree;
-  KDL::Chain youbot_kdl_chain;
-  std::string robot_description = node->get_parameter("robot_description").as_string();
-  if (!kdl_parser::treeFromString(robot_description, youbot_tree))
-  {
-    std::cout << "Unable to get parameters";
-  }
-  if (!youbot_tree.getChain("arm_link_0", "arm_link_5", youbot_kdl_chain))
-  {
-    std::cout << "Unable to ger chain";
-  }
-  manipulation_namespace::Manipulator manipulator;
-
-  // Assuming joint_angles is a vector of joint angles in radians
-  KDL::JntArray joint_angles(youbot_kdl_chain.getNrOfJoints());
-  joint_angles(0) = 0;  // Set joint angles as needed
-  joint_angles(1) = 0;
-  joint_angles(2) = 0;
-  joint_angles(3) = 0;
-  joint_angles(4) = 0;
-
-  KDL::Frame current_pose;
-
-  if (manipulator.forwardKinematics(joint_angles, youbot_kdl_chain, current_pose))
-  {
-    KDL::Frame target_pose;
-    target_pose.p = KDL::Vector(0.25732, 0.0849215, 0.2807);
-    target_pose.M = KDL::Rotation::Identity();  // Set orientation if needed
-
-    double amplitude = 0.01;
-
-    double velocity_x = calculateVelocity(amplitude, current_pose.p.x(),
-                                          target_pose.p.x(), current_pose.p.x());
-    double velocity_y = calculateVelocity(amplitude, current_pose.p.y(),
-                                          target_pose.p.y(), current_pose.p.y());
-    double velocity_z = calculateVelocity(amplitude, current_pose.p.z(),
-                                          target_pose.p.z(), current_pose.p.z());
-
-    std::cout << "Calculated Velocities:" << std::endl;
-    std::cout << "Velocity_x: " << velocity_x << std::endl;
-    std::cout << "Velocity_y: " << velocity_y << std::endl;
-    std::cout << "Velocity_z: " << velocity_z << std::endl;
-
-    KDL::ChainIkSolverVel_pinv solver(youbot_kdl_chain);
-
-    // Input the desired end-effector velocities
-    KDL::Twist desired_twist;
-    desired_twist.vel.x(velocity_x);
-    desired_twist.vel.y(velocity_y);
-    desired_twist.vel.z(velocity_z);
-
-    // Calculate joint velocities
-    KDL::JntArray joint_velocities(youbot_kdl_chain.getNrOfJoints());
-    solver.CartToJnt(joint_angles, desired_twist, joint_velocities);
-
-    std::cout << "Calculated Joint Velocities:" << std::endl;
-    for (int i = 0; i < joint_velocities.rows(); ++i)
-    {
-      double velocity_deg_per_s = joint_velocities(i) * (180.0 / M_PI);
-      std::cout << "Joint_" << i + 1 << ": " << velocity_deg_per_s << " deg/s"
-                << std::endl;
-    }
-  }
-  else
-  {
-    std::cerr << "Forward Kinematics failed." << std::endl;
-  }
-
   rclcpp::spin(node->get_node_base_interface());
   rclcpp::shutdown();
   return 0;
